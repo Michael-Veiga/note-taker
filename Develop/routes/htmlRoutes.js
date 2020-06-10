9f16655d1e11b20fdb75784efa4fa32395117378
Mike V <mvee333@gmail.com>
2020-06-10 17:11:38 -0400

    installed packages and got routes for htmlRoutes.js

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,9 +1,17 @@
 // install your packages which may include 'path', 'express' (router from express)
-
+const path = require("path");
+var router = require("express").Router();
 // get /notes route
-// sendFile() - directory, filename you want displayed
+app.get("/notes", function (req, res) {
+  // sendFile() - directory, filename you want displayed
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
 // get "*" route
-// sendFile() - directory, filename you want displayed
+app.get("*", function (req, res) {
+  // sendFile() - directory, filename you want displayed
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 // export the Router
+module.exports = router;

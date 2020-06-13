7fcd4cb038d7e289223df7adbe2f8468201cf021
Mike V <mvee333@gmail.com>
2020-06-13 19:47:27 -0400

    gave up on notebook.js and used functions in apiRoutes instead

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,25 +1,77 @@
 // require express
 const router = require("express").Router();
-// require the class file so you can use the functions that you created
-const notebook = require("../db/notebook");
+const path = require("path");
+const fs = require("fs");
+const db = path.join(__dirname, "../db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 // get request for /api/notes
 router.get("/notes", function (req, res) {
-  // call the getNotes() from the class that you required
-  notebook
-    .getNotes()
-    // res.json(notes)
-    .then((notes) => res.json(notes));
+  // read from data file using fs
+  fs.readFile(db, "utf-8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    // res.json(data)
+    res.json(JSON.parse(data));
+    console.log("notes have been returned");
+  });
 });
 
 // post request for /api/notes
-router.post("/notes", function (req, res) {
-  // call the addNotes() from the class that you required
-  notebook
-    .addNotes(req.body)
-    // res.json(note)
-    .then((note) => res.json(note));
+router.post("/notes", (req, res) => {
+  // create a variable that adds a note
+  let addNote = req.body;
+  // create an array for note title text and id
+  let notesArray = [];
+  // read from data file using fs
+  fs.readFile(db, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    notesArray = JSON.parse(data);
+    // use uuid to assign each new note
+    addNote.id = uuidv4();
+    // push the array to the addNote variable only after realizing an hour later that the push() function was fine, but you accidentally deleted your db.json
+    notesArray.push(addNote);
+    // write to the data file
+    fs.writeFile(db, JSON.stringify(notesArray), (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Your note has been added successfully");
+    });
+  });
+  res.json(addNote);
+});
+// delete request "notes/:id"
+router.delete("/notes/:id", (req, res) => {
+  let addNote = req.body;
+  let notesArray = [];
+  fs.readFile(db, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    notesArray = JSON.parse(data);
+    // create a variable to hold request parameters
+    let selector = req.params.id;
+    // filter the notesArray to single out any non-matching ids so that the right note is deleted
+    const findId = notesArray.filter((note) => {
+      // return the note id and selector
+      return note.id != selector;
+    });
+
+    // write to the data file with the updated information
+    fs.writeFile(db, JSON.stringify(findId), (err) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log("Your notes have been replaced");
+      }
+    });
+  });
+  res.json(addNote);
 });
 
-//  /api/notes/:id
-// call the deleteNote() function
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -4,11 +4,14 @@ const htmlRoutes = require("./routes/htmlRoutes");
 
 // initialize the app and create a port
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // set up middleware to handle post data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Use public folder
+app.use(express.static("public"));
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
